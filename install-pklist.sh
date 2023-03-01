da7dd67aeb543126d268021f6a00caf42d405f17
David7ce <79609649+David7ce@users.noreply.github.com>
2023-03-01 03:14:25 +0000

    remove duplicates from pkglist

--- install-pklist.sh
+++ install-pklist.sh
@@ -4,11 +4,11 @@
 sudo pacman -S plasma-desktop plasma-meta plasma-wayland-protocols plasma-wayland-session plasma-workspace
 
 # KDE extra
-sudo pacman -S ark digikam dolphin dolphin-plugins cuttlefish elisa filelight ffmpegthumbnailer ffmpegthumbs gwenview inkscape kamera kate kbackup kcachegrind kcalc kcron kdeconnect kdenetwork_filesharing kdenlive khelpcenter kmag kmix kmousetool kmouth kontrast kwrite konsole krita ksystemlog markdownpart okular partitionmanager print-manager skanlite skanpage spectacle sweeper yakuake
+sudo pacman -S ark digikam dolphin dolphin-plugins cuttlefish elisa filelight gwenview inkscape kamera kate kbackup kcachegrind kcalc kcron kdeconnect kdenetwork_filesharing kdenlive khelpcenter kmag kmix kmousetool kmouth kontrast kwrite konsole krita ksystemlog markdownpart okular partitionmanager print-manager skanlite skanpage spectacle sweeper yakuake
 
 # Extra packages
 # install packages with pacman
-sudo pacman -S archinstall arduino asciinema audacious base base-devel bleachbit blender calibre code chromium clonezilla ffmpeg ffmpegthumbnailer firefox firewalld flameshot flatpak freshfetch gimp git handbrake gimp godot gparted imagemagick kdenlive kepassxc kitty libreoffice-fresh libvirt mesa mpv neofetch neovim ntfs-3g obs-studio obsidian pacman-mirrorlist picard pipewire pipewire-alsa pipewire-audio pipewire-media-session pipewire-pulse qbitorrent rsync shotwell supertux telegram-desktop tree tmux virtualbox virt-install vlc wine xf86-video-amdgpu  xf86-video-nouveau  xf86-video-vesa xorg-server xorg-xwayland yt-dlp
+sudo pacman -S archinstall arduino asciinema audacious base base-devel bleachbit blender calibre code chromium clonezilla ffmpeg ffmpegthumbnailer ffmpegthumbs firefox firewalld flameshot flatpak freshfetch gimp git handbrake gimp godot gparted imagemagick kdenlive kepassxc kitty libreoffice-fresh libvirt mesa mpv neofetch neovim ntfs-3g obs-studio obsidian pacman-mirrorlist picard pipewire pipewire-alsa pipewire-audio pipewire-media-session pipewire-pulse qbitorrent rsync shotwell supertux telegram-desktop tree tmux virtualbox virt-install vlc wine xf86-video-amdgpu  xf86-video-nouveau  xf86-video-vesa xorg-server xorg-xwayland yt-dlp
 
 # install yay from aur with git
 git clone https://aur.archlinux.org/yay.git
