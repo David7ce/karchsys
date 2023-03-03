b91389f6f44a96247d8ebfdc226297fefbbd126b
David7ce <79609649+David7ce@users.noreply.github.com>
2023-03-03 00:31:08 +0000

    update packages list in post-installation

--- install-packages.sh
+++ install-packages.sh
@@ -1,15 +1,20 @@
 #! /bin/bash
 
+# Basic installation (Desktop Environment + display server + drivers)
 # kde plasma
-sudo pacman -S plasma-desktop plasma-meta plasma-wayland-protocols plasma-wayland-session plasma-workspace
+# sudo pacman -S plasma-desktop plasma-meta plasma-wayland-protocols plasma-wayland-session plasma-workspace
 
 # display server (xorg)
-sudo pacman -S xf86-video-amdgpu xf86-video-nouveau xf86-video-vesa xorg-server xorg-xwayland
+# sudo pacman -S xorg-server xorg-xwayland
 
 # audio drivers (pipewire)
-sudo pacman -S pipewire pipewire-alsa pipewire-audio pipewire-media-session pipewire-pulse 
+# sudo pacman -S pipewire pipewire-alsa pipewire-audio pipewire-media-session pipewire-pulse 
 
-# kde extra packages
+# video drivers
+# sudo pacman -S libva-mesa-driver nvidia xf86-video-amdgpu xf86-video-nouveau xf86-video-vesa xf86-video-ati-git xf86-video-chips
+
+# Post installation
+# kde packages
 sudo pacman -S ark digikam cuttlefish elisa filelight ffmpegthumbnailer ffmpegthumbs gwenview inkscape kamera kate kbackup kcachegrind kcalc kcron kdeconnect kdenetwork-filesharing kdenlive khelpcenter kmag kmix kmousetool kmouth kontrast kwrite konsole krita ksystemlog markdownpart okular partitionmanager print-manager skanlite skanpage spectacle sweeper yakuake
 
 # basic packages
@@ -20,7 +25,7 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 # install packages from AUR with yay
-yay -S 7zip-full activitywatch-bin autofirma-bin brew-git dia helvum-git libpamac libpamac-au libpamac-flatpak pamac-aur ungoogled-chromium upscayl-bin wlroots-git xampp xf86-video-ati-git xf86-video-chips youtube
+yay -S 7zip-full activitywatch-bin autofirma-bin brew-git dia helvum-git libpamac libpamac-au libpamac-flatpak pamac-aur ungoogled-chromium upscayl-bin wlroots-git xampp youtube
 
 # install packages with flatpak
 sudo flatpak install flathub com.usebottles.bottles me.hyliu.fluentreader net.veloren.airshipper com.github.tchx84.Flatseal net.davidotek.pupgui2 sh.ppy.osu io.lbry.lbry-app io.gitlab.theevilskeleton.Upscaler org.gimp.GIMP
