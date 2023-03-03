2b2b5b55cf87c0ae90168863730c9a021df134e0
David7ce <79609649+David7ce@users.noreply.github.com>
2023-03-03 14:42:22 +0000

    Update install-packages.sh

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -15,18 +15,19 @@ video drivers
 
 # Post installation
 # kde packages
-sudo pacman -S ark digikam cuttlefish elisa filelight ffmpegthumbnailer ffmpegthumbs gwenview inkscape kamera kate kbackup kcachegrind kcalc kcron kdeconnect kdenetwork-filesharing kdenlive khelpcenter kmag kmix kmousetool kmouth kontrast kwrite konsole krita ksystemlog markdownpart okular partitionmanager print-manager skanlite skanpage spectacle sweeper yakuake
+sudo pacman -S ark digikam elisa filelight ffmpegthumbnailer ffmpegthumbs gwenview inkscape kamera kate kbackup kcachegrind kcalc kcron kdeconnect kdenetwork-filesharing kdenlive khelpcenter kmag kmix kmousetool kmouth kontrast kwrite konsole krita ksystemlog markdownpart okular partitionmanager print-manager skanlite skanpage spectacle sweeper yakuake
 
 # basic packages
-sudo pacman -S archinstall arduino asciinema audacious base base-devel bleachbit blender calibre cava chromium clonezilla code curl docker docker-compose ffmpeg ffmpegthumbnailer firefox firewalld flameshot flatpak freshfetch git godot gparted handbrake imagemagick keepassxc kitty libreoffice-fresh libvirt libwebp lynx mesa mpv nb neofetch neovim ntfs-3g obs-studio obsidian pacman-mirrorlist picard qbittorrent qemu-base qemu-desktop reflector rsync snapper-support shotwell supertux telegram-desktop tree tmux unrar upscaler-bin virtualbox virt-install vlc wget wine yt-dlp
-# debtap dpkg gimp latte-dock lshw
+sudo pacman -S archinstall arduino asciinema audacious base base-devel bleachbit blender calibre chromium clonezilla code curl docker docker-compose ffmpeg ffmpegthumbnailer firefox firewalld flameshot flatpak git godot gparted handbrake imagemagick keepassxc kitty libreoffice-fresh libvirt libwebp lynx mesa mpv neofetch neovim ntfs-3g obs-studio obsidian pacman-mirrorlist picard qbittorrent qemu-base reflector rsync shotwell supertux telegram-desktop tree tmux unrar virt-install vlc wget wine yt-dlp
+# debtap dpkg freshfetch gimp latte-dock lshw nb qemu-desktop virtualbox
 
 # install yay from aur with git
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 # install packages from AUR with yay
-yay -S 7zip-full activitywatch-bin autofirma-bin dia helvum-git libpamac libpamac-au libpamac-flatpak pamac-aur ungoogled-chromium upscayl-bin wlroots-git xampp youtube
+yay -S 7zip-full activitywatch-bin  cava dia helvum-git snapper-support ungoogled-chromium upscayl-bin wlroots-git xampp youtube
+# autofirma-bin libpamac libpamac-au libpamac-flatpak pamac-aur
 
 # install packages with flatpak
 sudo flatpak install flathub com.usebottles.bottles me.hyliu.fluentreader net.veloren.airshipper com.github.tchx84.Flatseal net.davidotek.pupgui2 org.gimp.GIMP
