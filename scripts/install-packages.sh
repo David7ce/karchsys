4d4c44b0c2647788d48e500aea60f87100c62166
David7ce <79609649+David7ce@users.noreply.github.com>
2023-03-04 18:55:03 +0000

    add upower

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -5,7 +5,7 @@
 # sudo pacman -S plasma-desktop plasma-meta plasma-wayland-protocols plasma-wayland-session plasma-workspace
 
 # display server (xorg)
-# sudo pacman -S xorg-server xorg-xwayland
+# sudo pacman -S xorg-server xorg-xwayland upower
 
 # audio drivers (pipewire)
 # sudo pacman -S pipewire pipewire-alsa pipewire-audio pipewire-media-session pipewire-pulse 
@@ -16,7 +16,6 @@
 # Post installation
 # kde packages
 sudo pacman -S ark digikam elisa filelight ffmpegthumbnailer ffmpegthumbs gwenview inkscape kamera kate kbackup kcachegrind kcalc kcron kdeconnect kdenetwork-filesharing kdenlive khelpcenter kmag kmix kmousetool kmouth kontrast kwrite konsole krita ksystemlog markdownpart okular partitionmanager print-manager skanlite skanpage spectacle sweeper yakuake
-
 # basic packages
 sudo pacman -S archinstall arduino asciinema audacious base base-devel bleachbit blender calibre chromium clonezilla code curl docker docker-compose ffmpeg ffmpegthumbnailer firefox firewalld flameshot flatpak git godot gparted handbrake imagemagick keepassxc kitty libreoffice-fresh libvirt libwebp lynx mesa mpv neofetch neovim ntfs-3g obs-studio obsidian pacman-mirrorlist picard qbittorrent qemu-base reflector rsync shotwell supertux telegram-desktop tree tmux unrar virt-install vlc wget wine yt-dlp
 # debtap dpkg freshfetch gimp latte-dock lshw nb qemu-desktop virtualbox
