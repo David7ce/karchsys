a2fcb3f6d63805e113c57ff7c04ff92330626e88
David7ce <79609649+David7ce@users.noreply.github.com>
2023-03-12 03:28:19 +0000

    add aur-packages.txt flat-packages.txt and fix post-installation

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,20 +1,19 @@
 #! /bin/bash
 
-# install the list of packages with pacman
-# grep -v "^#" arch-packages.txt | xargs sudo pacman -S # directly ignore line comments
-cat arch-packages.txt | xargs sudo pacman -S
-
 # install yay from aur with git
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-# install packages from AUR with yay
-yay -S 7zip-full activitywatch-bin bottles dia helvum-git  ungoogled-chromium upscayl-bin xampp youtube-music-bin
-# btrfs-assistant snapper-support
-# autofirma-bin cava freetube-bin libpamac libpamac-au libpamac-flatpak pamac-aur wlroots-git
 
-# install packages with flatpak
-cat aur-packages.txt | xargs sudo flatpak install
+# install the list of packages with pacman
+# grep -v "^#" arch-packages.txt | xargs sudo pacman -S # directly ignore line comments
+cat ../packages/arch-packages.txt | xargs sudo pacman -S
+
+# install the list of packages with yay
+cat ../packages/aur-packages.txt | xargs sudo pacman -S
+
+# install the list packages with flatpak
+cat ../packages/flatpak-packages.txt | xargs sudo flatpak install
 
 # install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
